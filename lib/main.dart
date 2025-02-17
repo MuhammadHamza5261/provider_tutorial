fa8b9d6cdd3b41fc8b57220462f321ec7d4e350a
MeharHamza5261 <hamzafiaz708@gmail.com>
2025-02-18 02:28:58 +0500

    - slider and favourite items with provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_provider/provider/count_provider.dart';
 import 'package:flutter_provider/provider/favourite_provider.dart';
 import 'package:flutter_provider/provider/slider_provider.dart';
+import 'package:flutter_provider/provider/theme_changer_provider.dart';
 import 'package:flutter_provider/screens/count_screen.dart';
 import 'package:flutter_provider/screens/favourite/favourite_screen.dart';
 import 'package:flutter_provider/screens/slider_screen.dart';
+import 'package:flutter_provider/screens/theme/dark_theme.dart';
 import 'package:provider/provider.dart';
 
 
@@ -17,18 +19,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CounterProvider()),
         ChangeNotifierProvider(create: (_) => SliderProvider()),
         ChangeNotifierProvider(create: (_) => FavouriteProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeChangerProvider()),
 
       ],
         
-         child: const MaterialApp(
-           debugShowCheckedModeBanner: false,
-           home: FavouriteScreen(),
-         ),
+         child:  Builder(
+             builder: (BuildContext context){
+               final themeChanger = Provider.of<ThemeChangerProvider>(context);
+               return MaterialApp(
+                 debugShowCheckedModeBanner: false,
+                 themeMode: themeChanger.themeMode,
+                 // for use primary
+                 theme: ThemeData(
+                   brightness: Brightness.light ,
+                     primarySwatch: Colors.blue,
+                 ),
+                 darkTheme: ThemeData(
+                   brightness: Brightness.dark,
+                   primarySwatch: Colors.purple
+                 ),
+                 home: DarkTheme(),
+               );
+             }
+         )
     );
   }
 }
